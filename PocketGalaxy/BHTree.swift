1a4f3f314eeb8b62ab7be275acd34bd08fd3cec9
Martin Wan <mlwan@uvic.ca>
2016-05-23 15:02:20 -0700

    Prepared architecture for program, about to implement functions

---
+++ PocketGalaxy/BHTree.swift
@@ -0,0 +1,13 @@
+//
+//  BHTree.swift
+//  PocketGalaxy
+//
+//  Created by Martin  on 2016-05-23.
+//  Copyright © 2016 Martin . All rights reserved.
+//
+
+import UIKit
+
+class BHTree: SKNode {
+
+}
---
+++ PocketGalaxy/QuadrantNode.swift
@@ -0,0 +1,13 @@
+//
+//  QuadrantNode.swift
+//  PocketGalaxy
+//
+//  Created by Martin  on 2016-05-23.
+//  Copyright © 2016 Martin . All rights reserved.
+//
+
+import Cocoa
+
+class QuadrantNode: SKScene {
+
+}
