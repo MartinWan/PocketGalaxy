835ab5c11c7987095db2121f05a89228ddff0297
Martin Wan <mlwan@uvic.ca>
2016-05-23 22:53:25 -0700

    progress towards working BH Algorithm

--- PocketGalaxy/GameScene.swift
+++ PocketGalaxy/GameScene.swift
@@ -11,67 +11,76 @@ import SpriteKit
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
     var particles = Array<Particle>()
+    var root = QuadrantNode(topRight: CGPoint(), bottomLeft: CGPoint()) // temp fix to avoid use of game scene initializers
     
     override func didMoveToView(view: SKView) {
         
+        let sceneTopRight = CGPoint(x: size.width , y: size.height)
+        let sceneBottomleft = CGPoint(x: 0, y: 0)
+        root = QuadrantNode(topRight: sceneTopRight, bottomLeft: sceneBottomleft)
+
         physicsWorld.contactDelegate = self
+    
         
-        let N = 500
+        let particle = Particle()
+        particle.position = CGPoint(x: 0, y: 0) // SW
+        root.insert(particle)
+        
+        let particle1 = Particle()
+        particle.position = CGPoint(x: 1, y: 1) // SW
+        root.insert(particle1)
+        
+        /*
+        let N = 10
+        let M = CGFloat(N) * Particle().mass
+        
         for _ in 1...N {
             
             let particle = Particle()
             
-            // generate R from exponential distribution, Vancouleur's law
+            // generate R from exponential distribution
             let u = Float(arc4random()) / Float(UINT32_MAX)
-            let lambda = Float(0.001)
+            let lambda = Float(0.01)
             let R = -log(1.0 - u) / lambda
             
             // generate random angle
-            let angle = 2.0 * Float(M_PI) * Float(arc4random()) / Float(UINT32_MAX)
+            let angle = Float(arc4random()) % Float(2 * M_PI)
             
             // init particle position
-            let rx = R * cos(angle) + Float(size.width / 2)
-            let ry = R * sin(angle) + Float(size.height / 2)
+            let rx = R * cos(Float(angle)) + Float(size.width / 2)
+            let ry = R * sin(Float(angle)) + Float(size.height / 2)
             particle.position = CGPoint(x: Int(rx), y: Int(ry))
             
             // init particle velocity
-            let M = particle.mass * CGFloat(N) // total mass
-            let V = sqrt(M / CGFloat(sqrt(R)))
+            let V = sqrt(M / CGFloat(R))
             let vx = -V * CGFloat(sin(angle))
             let vy = V * CGFloat(cos(angle))
             particle.velocity = CGVector(dx: Double(vx), dy: Double(vy))
             
+            root.insert(particle)
             
+            // add particle to scene
             particles.append(particle)
             self.addChild(particle)
         }
- 
-        /* Two body orbit test
-        let p1 = Particle()
-        let p2 = Particle()
- 
-        p1.position = CGPoint(x: 300, y: 300)
-        p2.position = CGPoint(x: 300, y: 400)
-        p2.velocity = CGVector(dx: 100, dy: 0)
-        p1.mass = pow(10, 6)
- 
-        particles.append(p1)
-        particles.append(p2)
-        self.addChild(p1)
-        self.addChild(p2)
+        
         */
+        
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
-        // TODO: implement proper inelastic collisions?
+       // intentionally blank for now
     }
 
     override func update(currentTime: NSTimeInterval) {
         
-        
         let epsilon = CGFloat(0.01) // for singularity softening
         let h = CGFloat(0.01) // size of runge kutta h-step, error is O(h^4)
         var newParticleVelocities = Dictionary<Particle, CGVector>()
+        
+        // TODO: for erase below, for all particles: calculate force due to other particles 
+        // i.e. for all particles: root.getForceOnParticle(particle) -> change-list[particle]
+        // then ... for all changes, update particles in scene
         
         // calculate new particle velocities
         for particle in particles {
@@ -87,10 +96,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 let Rx = otherParticle.position.x - particle.position.x
                 let Ry = otherParticle.position.y - particle.position.y
                 let R = sqrt(Rx * Rx + Ry * Ry)
-                
-                // CHANGED TO 1 / R
-                gx += (mass * Rx) / (R * R + epsilon)
-                gy += (mass * Ry) / (R * R + epsilon)
+   
+                gx += (mass * Rx) / (R * R * R + epsilon)
+                gy += (mass * Ry) / (R * R * R + epsilon)
             }
             // calculate particle's new velocity
             let vx = particle.velocity.dx + h * gx
--- PocketGalaxy/QuadrantNode.swift
+++ PocketGalaxy/QuadrantNode.swift
@@ -6,8 +6,165 @@
 //  Copyright © 2016 Martin . All rights reserved.
 //
 
-import Cocoa
+import SpriteKit
 
-class QuadrantNode: SKScene {
+enum Quadrant:Int {
+    case NW = 0
+    case NE = 1
+    case SW = 2
+    case SE = 3
+}
 
+class QuadrantNode {
+
+    var subquadrants = Dictionary<Quadrant, QuadrantNode>()
+    var particle:Particle?
+    
+    var quadTopRight:CGPoint
+    var quadBottomLeft:CGPoint
+    
+    var mass:CGFloat
+    var centerOfMass:CGPoint?
+    var isLeaf = false
+    
+    init(topRight: CGPoint, bottomLeft: CGPoint) {
+        quadTopRight = topRight
+        quadBottomLeft = bottomLeft
+        mass = 0
+        particleCount = 0
+    }
+    
+    func insert(particle: Particle) {
+        
+        let quadrant = getQuadrant(particle)
+        let subQuadrantTopRight = getSubQuadrantTopRight(particle)
+        let subQudrantBottomLeft = getSubQuadrantBottomLeft(particle)
+        
+        if subquadrants[quadrant] == nil {
+            
+            subquadrants[quadrant] = QuadrantNode(topRight: subQuadrantTopRight, bottomLeft: subQudrantBottomLeft)
+            subquadrants[quadrant]!.particle = particle
+            subquadrants[quadrant]!.isLeaf = true
+            isLeaf = false
+
+        } else if subquadrants[quadrant] != nil && subquadrants[quadrant]!.isLeaf { // quadrant has exactly 1 child
+            
+            let existingParticle = subquadrants[quadrant]!.particle! // must have particle since is leaf node
+            
+            // remove particle from the leaf node
+            subquadrants[quadrant]!.particle = nil
+            subquadrants[quadrant]!.isLeaf = false
+            
+            // recursively insert existing particle and new particle
+            subquadrants[quadrant]!.insert(existingParticle)
+            subquadrants[quadrant]!.insert(particle)
+            
+        } else { // more than 1 subquadrants already
+            
+            subquadrants[quadrant]!.insert(particle)
+        }
+        
+    }
+    
+    func getQuadrant(particle: Particle) -> Quadrant {
+        let width = quadTopRight.x - quadBottomLeft.x
+        let height = quadTopRight.y - quadBottomLeft.y
+        
+        let quadMidX = quadBottomLeft.x + width / 2
+        let quadMidY = quadBottomLeft.y + height / 2
+        
+        if (particle.position.x < quadMidX && particle.position.y < quadMidY) {
+            return Quadrant.SW
+        } else if (particle.position.x < quadMidX && particle.position.y > quadMidY) {
+            return Quadrant.NW
+        } else if (particle.position.x > quadMidX && particle.position.y < quadMidY) {
+            return Quadrant.SE
+        } else {
+            return Quadrant.NE
+        }
+    }
+    
+    func getSubQuadrantTopRight(particle: Particle) -> CGPoint {
+        let width = quadTopRight.x - quadBottomLeft.x
+        let height = quadTopRight.y - quadBottomLeft.y
+        
+        let quadMidX = quadBottomLeft.x + width / 2
+        let quadMidY = quadBottomLeft.y + height / 2
+        
+        if (particle.position.x < quadMidX && particle.position.y < quadMidY) {
+            return CGPoint(x: quadMidX, y: quadMidY)
+        } else if (particle.position.x < quadMidX && particle.position.y > quadMidY) {
+            return CGPoint(x: quadMidX, y: quadTopRight.y)
+        } else if (particle.position.x > quadMidX && particle.position.y < quadMidY) {
+            return CGPoint(x: quadTopRight.x, y: quadMidY)
+        } else {
+            return quadTopRight
+        }
+    }
+    
+    func getSubQuadrantBottomLeft(particle: Particle) -> CGPoint {
+        let width = quadTopRight.x - quadBottomLeft.x
+        let height = quadTopRight.y - quadBottomLeft.y
+        
+        let quadMidX = quadBottomLeft.x + width / 2
+        let quadMidY = quadBottomLeft.y + height / 2
+        
+        if (particle.position.x < quadMidX && particle.position.y < quadMidY) {
+            return quadBottomLeft
+        } else if (particle.position.x < quadMidX && particle.position.y > quadMidY) {
+            return CGPoint(x: quadBottomLeft.x, y: quadMidY)
+        } else if (particle.position.x > quadMidX && particle.position.y < quadMidY) {
+           return CGPoint(x: quadMidX, y: quadBottomLeft.y)
+        } else {
+            return CGPoint(x: quadMidX, y: quadMidY)
+        }
+    }
+    
+    func getSubSubQuadrantTopRight(particle: Particle) -> CGPoint {
+        let width = quadTopRight.x - quadBottomLeft.x
+        let height = quadTopRight.y - quadBottomLeft.y
+        
+        let quadMidX = quadBottomLeft.x + width / 2
+        let quadMidY = quadBottomLeft.y + height / 2
+        
+        if (particle.position.x < quadMidX && particle.position.y < quadMidY) {
+            return CGPoint(x: quadMidX, y: quadMidY)
+        } else if (particle.position.x < quadMidX && particle.position.y > quadMidY) {
+            return CGPoint(x: quadMidX, y: quadTopRight.y)
+        } else if (particle.position.x > quadMidX && particle.position.y < quadMidY) {
+            return CGPoint(x: quadTopRight.x, y: quadMidY)
+        } else {
+            return quadTopRight
+        }
+    }
+    
+    func getSubSubQuadrantBottomLeft(particle: Particle) -> CGPoint {
+        let width = quadTopRight.x - quadBottomLeft.x
+        let height = quadTopRight.y - quadBottomLeft.y
+        
+        let quadMidX = quadBottomLeft.x + width / 2
+        let quadMidY = quadBottomLeft.y + height / 2
+        
+        if (particle.position.x < quadMidX && particle.position.y < quadMidY) {
+            return quadBottomLeft
+        } else if (particle.position.x < quadMidX && particle.position.y > quadMidY) {
+            return CGPoint(x: quadBottomLeft.x, y: quadMidY)
+        } else if (particle.position.x > quadMidX && particle.position.y < quadMidY) {
+            return CGPoint(x: quadMidX, y: quadBottomLeft.y)
+        } else {
+            return CGPoint(x: quadMidX, y: quadMidY)
+            
+        }
+    }
+    
+    
+    
+    func computeMassDistribution() {
+        
+    }
+    
+    func getForceOnParticle(targetParticle:Particle) {
+        
+    }
+    
 }
